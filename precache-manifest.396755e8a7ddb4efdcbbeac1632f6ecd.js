17a0441e6483a54d21f9d22ef48940d0f15f7629
Tamilselvan Sellappan <48302@hexaware.com>
2020-10-02 21:52:28 +0530

    Updates

---
+++ precache-manifest.396755e8a7ddb4efdcbbeac1632f6ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d00c9b3e3dcd4318c3ed56be606e24",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b6c7598143841d946b57",
+    "url": "/portal/static/css/main.20258812.chunk.css"
+  },
+  {
+    "revision": "82532f8db34bcbd6c669",
+    "url": "/portal/static/js/2.e2f69403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portal/static/js/2.e2f69403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c7598143841d946b57",
+    "url": "/portal/static/js/main.21887317.chunk.js"
+  },
+  {
+    "revision": "0b42840507d607c1a70a",
+    "url": "/portal/static/js/runtime-main.dedadb5d.js"
+  }
+]);
